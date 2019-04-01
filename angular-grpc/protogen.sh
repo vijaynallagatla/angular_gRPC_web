5742d9437123ed58e4fc508ea90eae275aaa3f7b
vikumarn <vikumarn>
2019-04-01 15:28:29 +0530

    Added gRPC service to Angular project

--- angular-grpc/protogen.sh
+++ angular-grpc/protogen.sh
@@ -1,21 +1,11 @@
-#!/bin/bash
-
-mkdir -p ./ts/_proto
-
-PROTOC=`command -v protoc`
-if [[ "$PROTOC" == "" ]]; then
-  echo "Required "protoc" to be installed. Please visit https://github.com/protocolbuffers/protobuf/releases (3.5.0 suggested)."
-  exit -1
-fi
-
-# Path to this plugin, Note this must be an abolsute path on Windows (see #15)
-PROTOC_GEN_TS_PATH="/node_modules/.bin/protoc-gen-ts"
+# Path to this plugin
+PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 
 # Directory to write generated code to (.js and .d.ts files)
-OUT_DIR="/generated"
+OUT_DIR="./proto/ts/"
 
 protoc \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out="import_style=commonjs,binary:${OUT_DIR}" \
     --ts_out="service=true:${OUT_DIR}" \
-    echo.proto
+    ./proto/echo.proto
--- angular-grpc/src/app/app.component.ts
+++ angular-grpc/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EchoServiceClient } from 'proto/ts/echo_pb_service';
+import { EchoRequest } from 'proto/ts/echo_pb';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-grpc';
+  public echoService = new EchoServiceClient('http://localhost:8080');
+  ngOnInit(): void {
+    const request = new EchoRequest();
+    request.setMessage('Hello World!');
+
+    this.echoService.echo(request, {} as any, function(err, response) {
+      console.log(response);
+    });
+  }
 }
